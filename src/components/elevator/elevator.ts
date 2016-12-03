f1a0a4411773f0453d9ed3225558ab66618d9140
FazioNico <contact@nicolasfazio.ch>
2016-12-03 17:16:53 +0100

    mve google import SDK into GoogleMapService

--- src/components/elevator/elevator.ts
+++ src/components/elevator/elevator.ts
@@ -17,84 +17,18 @@ export class ElevatorComponent {
   text:string;
   elevation:number;
   mapInitialised:boolean = false;
-  apiKey:string // = 'YOUR_OWN_GOOGLE_MAP_API_KEY';
 
   constructor(
     private _ngZone: NgZone
   ) {
-    this.loadGoogleMaps();
+    //this.loadGoogleMaps();
   }
 
-    /* Google Map loading & Initiallisation */
-    loadGoogleMaps():void{
-       this.addConnectivityListeners();
-       if(typeof google == "undefined" || typeof google.maps == "undefined"){
-         //console.log("Google maps JavaScript needs to be loaded.");
-         if(navigator.onLine === true){
-           //console.log("online, loading map");
-           //Load the SDK with the callback
-           window['mapInit'] = () => {
-             this.initMap();
-           }
-           let script:HTMLScriptElement = document.createElement("script");
-           script.id = "googleMaps";
-           script.async = true;
-           if(this.apiKey){
-             script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
-           } else {
-             script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
-           }
-           document.body.appendChild(script);
-         }
-       }
-       else {
-         if(navigator.onLine === true){
-           //console.log("map ready");
-           this.initMap();
-         }
-         else {
-           this.disableMap();
-         }
-       }
-     }
-
-     /* Events Connectivity listener for Google Map */
-     addConnectivityListeners():void{
-        let onOnline = () => {
-          setTimeout(()=> {
-            if(typeof google == "undefined" || typeof google.maps == "undefined"){
-              this.loadGoogleMaps();
-            }
-            else {
-              if(!this.mapInitialised){
-                this.initMap();
-              }
-            }
-          }, 1000);
-        };
-        let onOffline = ()=> {
-          this.disableMap();
-        };
-        document.addEventListener('online', _=> onOnline, false);
-        document.addEventListener('offline', _=> onOffline, false);
-    }
-
-    /* Google Map Core Methodes */
-    disableMap():void{
-      // here you can do somthing...
-    }
-
-    initMap():void {
-      this.mapInitialised = true;
-      setTimeout(()=>{
-        // console.log('google SDK init-> ', google)
-      },100)
-    }
 
     /* Elevator API  Methode */
     displayLocationElevation(location:any):void {
       //console.log('test search-> ', this.mapInitialised)
-      if(this.mapInitialised === false || !location.coords){
+      if(google === false || !location.coords){
         return null;
       }
       //console.log('search', location)
--- src/providers/google-map-service.ts
+++ src/providers/google-map-service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+declare var google;
+
 /*
   Generated class for the GoogleMapService provider.
 
@@ -11,8 +13,77 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GoogleMapService {
 
+  apiKey:string // = 'YOUR_OWN_GOOGLE_MAP_API_KEY';
+  mapInitialised:boolean = false;
+
   constructor(public http: Http) {
     console.log('Hello GoogleMapService Provider');
+    this.loadGoogleMaps()
   }
 
+  /* Google Map loading & Initiallisation */
+  loadGoogleMaps():void{
+     this.addConnectivityListeners();
+     if(typeof google == "undefined" || typeof google.maps == "undefined"){
+       //console.log("Google maps JavaScript needs to be loaded.");
+       if(navigator.onLine === true){
+         //console.log("online, loading map");
+         //Load the SDK with the callback
+         window['mapInit'] = () => {
+           this.initMap();
+         }
+         let script:HTMLScriptElement = document.createElement("script");
+         script.id = "googleMaps";
+         script.async = true;
+         if(this.apiKey){
+           script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+         } else {
+           script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+         }
+         document.body.appendChild(script);
+       }
+     }
+     else {
+       if(navigator.onLine === true){
+         //console.log("map ready");
+         this.initMap();
+       }
+       else {
+         this.disableMap();
+       }
+     }
+   }
+
+   /* Events Connectivity listener for Google Map */
+   addConnectivityListeners():void{
+      let onOnline = () => {
+        setTimeout(()=> {
+          if(typeof google == "undefined" || typeof google.maps == "undefined"){
+            this.loadGoogleMaps();
+          }
+          else {
+            if(!this.mapInitialised){
+              this.initMap();
+            }
+          }
+        }, 1000);
+      };
+      let onOffline = ()=> {
+        this.disableMap();
+      };
+      document.addEventListener('online', _=> onOnline, false);
+      document.addEventListener('offline', _=> onOffline, false);
+  }
+
+  /* Google Map Core Methodes */
+  disableMap():void{
+    // here you can do somthing...
+  }
+
+  initMap():void {
+    this.mapInitialised = true;
+    setTimeout(()=>{
+      console.log('google SDK init-> ', google)
+    },100)
+  }
 }
